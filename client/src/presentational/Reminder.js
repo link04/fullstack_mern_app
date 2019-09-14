cdc27851d90e27316b02d937e00ae953a77be75d
Maximo Bautista <max.bautista97@gmail.com>
2019-09-13 23:46:41 -0400

    Added window confirm for delete button.

--- client/src/presentational/Reminder.js
+++ client/src/presentational/Reminder.js
@@ -7,14 +7,13 @@ const Reminder = (props) => {
       <td>{props.reminder.id}</td>
       <td>{props.reminder.message}</td>
       <td>
-        <button className="delete" onClick={() => props.deleteHandler(props.reminder.id)}>
+        <button className="delete" onClick={() =>
+            window.confirm("Do you really want to delete this?") ?
+            props.deleteHandler(props.reminder.id) : null} >
           Delete
         </button>
-        <button className="update"
-            onClick={() =>
-              props.updateDB(this.state.idToUpdate, this.state.updateToApply)
-            }
-          >
+        <button className="update" onClick={() =>
+            props.updateDB(this.state.idToUpdate, this.state.updateToApply)} >
           Update
         </button>
       </td>
